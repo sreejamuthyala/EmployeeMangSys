f84bd28b60359ae1c88041314818731ce7560dab
sreeja <sreejareddym12@gmail.com>
2022-01-22 16:30:25 +0530

    added space

--- Models/Departments.cs
+++ Models/Departments.cs
@@ -2,8 +2,8 @@ namespace EmployeeManagementSystem.Models
 {
     public class Departments
     {
-        public int DepartmentId{get; set;}
+        public int DepartmentId{get; set;} 
         public string DepartmentName{get; set;}
-        public string DepartmentLocation{get; set;}
+        public string DepartmentLocation{get; set;} 
     }
 }
