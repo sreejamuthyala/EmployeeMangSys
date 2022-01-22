e99e9539ad64b2faa512f0f7cca078169d723ed2
Swarnaganesh <swarna1455@gmail.com>
2022-01-22 16:26:57 +0530

    added space

--- Models/Companies.cs
+++ Models/Companies.cs
@@ -4,8 +4,8 @@ namespace EmployeeManagementSystem.Models
     {
         public int CompanyId{get; set;}
         public string CompanyName{get; set;}
-        public string CompanyAddress{get; set;}
-        public string City{get; set;}
+        public string CompanyAddress{get; set;} 
+        public string City{get; set;} 
         public int PostalCode{get; set;}
         public string Country{get; set;}
     }
--- Models/Employees.cs
+++ Models/Employees.cs
@@ -2,18 +2,18 @@ namespace EmployeeManagementSystem.Models
 {
     public class Employees
     {
-        public int EmployeeId{get; set;}
+        public int EmployeeId{get; set;}  
         public string LastName{get; set;}
         public string FirstName{get; set;}
-        public DateTime BirthDate{get; set;}
+        public DateTime BirthDate{get; set;}  
         public DateTime HireDate{get; set;}
         public string Address{get; set;}
-        public string City{get; set;}
+        public string City{get; set;}  
         public string Region{get; set;}
         public int PostalCode{get; set;}
         public string Country{get; set;}
-        public int HomePhone{get; set;}
-        public int DepartmentId{get; set;}
-        public int ProjectId{get; set;}
+        public int HomePhone{get; set;}    
+        public int DepartmentId{get; set;}  
+        public int ProjectId{get; set;}      
     }
 }
