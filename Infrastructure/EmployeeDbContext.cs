7f0e8ce53bb9b3e292de8dd2dd3e9627913642e0
sreeja <sreejareddym12@gmail.com>
2022-01-22 20:43:05 +0530

    created company controlller

--- Infrastructure/EmployeeDbContext.cs
+++ Infrastructure/EmployeeDbContext.cs
@@ -6,7 +6,7 @@ namespace EmployeeManagementSystem.Infrastructure
     public class EmployeeDbContext : DbContext
     {
         public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options) { }
-        my name is sreeja
+      
         public DbSet<Employees> Employee {get; set;}
     }
 }
