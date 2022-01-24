54f67a269c5f44420e6eaeef652995fcdd3c7d0b
sreeja <sreejareddym12@gmail.com>
2022-01-24 12:41:29 +0530

    CompaniesCintroller modified

--- Controllers/CompaniesController.cs
+++ Controllers/CompaniesController.cs
@@ -11,27 +11,64 @@ namespace EmployeeManagementSystem.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CompaniesController : ControllerBase
+
+    public class CompanyController : ControllerBase
+
     {
-         ICRUDRepository<Companies, int> _repository;
-        public CompaniesController(ICRUDRepository<Companies, int> repository) => _repository = repository;
+      ICRUDRepository<Companies, int> _repository; 
+        public CompanyController( ICRUDRepository<Companies, int> repository )  => _repository = repository;
         public ActionResult<IEnumerable<Companies>> Get()
         {
-            var items = _repository.GetAll();
+            var items = _repository.GetAll(); 
             return items.ToList();
         }
+        //Add the EFCore.SQLServer package 
+        //dotnet add package Microsoft.EntityFrameworkCore.SqlServer
 
-        [HttpGet("Company")]
-        public Models.Companies GetCompany()
+
+         //URL: /api/employees/1
+         //try with id parameter values between 1 and 9
+
+         [HttpGet("{id}")]
+         public ActionResult<Companies> GetDetails(int id)
         {
-            Models.Companies obj = new Models.Companies{
-                CompanyId=1,
-                CompanyName="Capgemini",
-                
-            };
-           return obj;  
+            var item = _repository.GetDetails(id);
+            if(item==null)
+            return NotFound();
+
+            return item;
         }
-       
+         [HttpPost("addnew")]
+        public ActionResult<Companies> Create(Companies emp)
+       {
+           if(emp==null)
+           
+             return BadRequest();
+
+
+             _repository.Create(emp);
+              return emp;
+  
+            
+       }
+
+       [HttpPut("update/{id}")]
+       public ActionResult<Companies> update(int id, Companies emp)
+       {
+           if(emp==null)
+             return BadRequest();
+            _repository.update(emp);
+            return emp;
+       }
+       [HttpDelete("remove/{id}")]
+       public ActionResult Delete(int id)
+       {
+           _repository.Delete(id);
+
+            return Ok();
+       }
+
+
     }
+
 }
-    
